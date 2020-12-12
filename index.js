3e6de937849626f5b26a30f0a1b8c9518078ca26
klesson <49154904+klesson@users.noreply.github.com>
2020-12-12 12:58:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,28 +10,34 @@ const firebaseConfig = {
 
 var list = document.getElementById('list');
 var text = document.getElementById('text');
+var button = document.getElementById('submit');
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var collection = firebase.firestore().collection('list');
 
-function addItem(value, id) {
+function addItem(value, id, animation, canRemove) {
     const item = document.createElement('li');
     const itemText = document.createElement('span');
     itemText.textContent = value;
-    const itemClose = document.createElement('span');
-    itemClose.textContent = 'x';
-    itemClose.setAttribute('index', id);
-    itemClose.addEventListener('click', remove);
     item.appendChild(itemText);
-    item.appendChild(itemClose);
-    item.classList.add('animate__fadeInUp');
-    item.classList.add('animate__animated');
-    list.appendChild(item);
+    if (canRemove) {
+        const itemClose = document.createElement('span');
+        itemClose.textContent = 'x';
+        itemClose.setAttribute('index', id);
+        itemClose.setAttribute('title', 'Удалить сообщение');
+        itemClose.addEventListener('click', remove);
+        item.appendChild(itemClose);
+    }
+    if (animation) {
+        item.classList.add(animation);
+        item.classList.add('animate__animated');
+    }
+    list.prepend(item);
 }
 
 collection.orderBy('time').get().then(snapshot => {
     snapshot.forEach(doc => {
-        addItem(doc.data().title, doc.id);
+        addItem(doc.data().title, doc.id, 'animate__fadeIn');
     });
 });
 
@@ -39,8 +45,13 @@ function add(event) {
     event.preventDefault();
     if (text.value) {
         collection.add({title: text.value, time: Date.now()}).then(res => {
-            addItem(text.value, res.id);
+            addItem(text.value, res.id, 'animate__fadeInDown', true);
             text.value = '';
+            button.classList.add('animate__animated', 'animate__bounce');
+            button.addEventListener('animationend', () => {
+                button.classList.remove('animate__animated');
+                button.classList.remove('animate__bounce');
+            });
         });
     }
 }
@@ -48,7 +59,7 @@ function add(event) {
 function remove(event) {
     var index = event.target.getAttribute('index')
     var li = event.target.parentElement;
-    li.classList.remove('animate__fadeInUp');
+    //li.classList.remove('animate__fadeInDown');
     li.classList.add('animate__fadeOutLeft');
     setTimeout(() => {
         li.remove();
